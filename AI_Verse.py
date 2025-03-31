99802d296155a4df6aac9be3dabbf5278b78dc18
Vedika Rajashekar <vedikarajashekar@gmail.com>
2025-03-31 12:10:31 +0530

    Update AI_Verse.py

--- AI_Verse.py
+++ AI_Verse.py
@@ -106,4 +106,4 @@ def processing_():
         return jsonify({"error": "File format not supported"}), 400
     
 if __name__ == "__main__":
-    app.run(debug= True, host= "0.0.0.0", port= 5000)
+    app.run(debug= True, host= "0.0.0.0", port= 10000)
